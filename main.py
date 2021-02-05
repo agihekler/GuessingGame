95676a4d0e7c7afd467c32b8404c1dbb811fe4fe
agihekler <agnes.hekler@gmail.com>
2021-02-05 18:57:20 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+
+def main():
+  print("Welcome to the Guessing Game.")
+  print("The object of the game is to guess a number between 1 and 1000 within 5 tries.")
+
+if __name__ == "__main__":
+  main()
+  
+  
